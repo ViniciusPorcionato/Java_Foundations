1e5e217c40dcffba23937fb89544e1c5d1f0b9dc
Vinicius Porcionato <47423489824@info.local>
2024-08-24 12:04:38 -0300

    Variables JAVA

---
+++ Java Oracle/Java Projects/MyFirstProject/src/myfirstproject/MyFirstProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myfirstproject;
+
+/**
+ *
+ * @author 47423489824
+ */
+public class MyFirstProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello again world");
+    }
+    
+}
---
+++ Js/Example.js
@@ -0,0 +1,17 @@
+n1 = parseFloat(prompt("Digite a primeira nota"))
+n2 = parseFloat(prompt("Digite a segunda nota"))
+n3 = parseFloat(prompt("Digite a tercceira nota"))
+
+media = (n1 + n2 + n3) / 3
+
+if (media >= 7) {
+    alert("Aprovado")
+}else if (media = 6){
+    alert("Recuperação")
+}else{
+    alert("Reprovado")
+}
+
+function media() {
+    
+}
